e5606c9bcc1a4377452d4deb5f4dc4f23f2f9d2b
Renan Dias <129848338+RenanGDias@users.noreply.github.com>
2025-05-19 23:53:06 -0300

    Conhecendo diferentes formas de estilizar componentes CSS

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,15 @@
-import Card from './Card.jsx'
+// Como estilizar componentes React com CSS
+// -------------------------------------------
+// Sem incluir pre-processadores ou frameworks
+
+// 1. Externo (bom para estilos globais ou projetos pequenos)
+// 2. Módulos (bom para componentes individuais com estilos únicos)
+// 3. Em linha (bom para componentes pequenos com estilos mínimos)
+
+import Button from './Button.jsx'
 
 function App() {
-    return(
-        <>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-        </>
-    );
+    return(<Button/>);
 }
 
 export default App
