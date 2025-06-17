8af5446b6c95488096951652641441c46d204af0
Renan Dias <129848338+RenanGDias@users.noreply.github.com>
2025-06-17 17:26:40 -0300

    Utilizando o manipulador de eventos onChange

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,10 @@
-// useState() = Permite a criação de uma variável com estado (stateful) e uma função setter para atualizar seu valor no Virtual DOM
 
-
-import Counter from "./Counter.jsx";
 
 import MyComponent from "./MyComponent.jsx";
 
 function App() {
     
-    return (<Counter />);
-    // return (<MyComponent />);
+    return (<MyComponent />);
 }
 
 export default App
