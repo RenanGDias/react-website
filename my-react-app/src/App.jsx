d18fd745f8a53ef8dff9f2cd3737c0bdc449d7de
Renan Dias <129848338+RenanGDias@users.noreply.github.com>
2025-06-25 15:06:51 -0300

    Aprendendo a usar o useContext() em React para passagem de valores entre classes relacionadas

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,10 @@
-import DigitalClock from "./DigitalClock.jsx";
+// useContext() = Gancho do React (hook) que permite o compartilhamento de valores entre diferentes níveis de componentes sem a necessidade de passá-los através de cada nível
+
+import ComponentA from "./ComponentA";
 
 function App() {
     
-    return (<DigitalClock />);
+    return (<ComponentA />);
 }
 
 export default App
